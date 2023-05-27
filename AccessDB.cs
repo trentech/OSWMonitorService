2b3ec3f2dc9bc12d7b16c8d73cb282a52c1e2e27
TrenTech <trentech@gmail.com>
2023-05-26 20:11:55 -0400

    Organize code

--- AccessDB.cs
+++ AccessDB.cs
@@ -1,50 +1,11 @@
-﻿using Microsoft.Extensions.Logging;
-using NPOI.POIFS.Crypt.Dsig;
-using NPOI.Util;
-using System.Data;
+﻿using System.Data;
 using System.Data.OleDb;
 
 namespace OSWMontiorService
 {
-    public class DBTest : BackgroundService
+    public class AccessDB
     {
-        private readonly ILogger<DBTest> logger;
-
-        public DBTest(ILogger<DBTest> logger)
-        {
-            this.logger = logger;
-        }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            logger.LogInformation("Starting Dev Service: {time}", DateTime.Now);
-
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                logger.LogInformation("Running Task: {time}", DateTime.Now);
-
-                Sensor sensor = new Sensor("TEST", "192.168.1.1");
-                sensor.Temperature = 55.5;
-                sensor.Humidity = 30.45;
-                sensor.DewPoint = 35;
-                sensor.IsRecording = true;
-                sensor.Date = DateOnly.FromDateTime(DateTime.Now);
-                sensor.Time = TimeOnly.FromDateTime(DateTime.Now);
-
-                if(!TableExists(sensor))
-                {
-                    CreateTable(sensor);
-                }
-
-                AddSensorData(sensor);
-
-                logger.LogInformation("Waiting 60 seconds: {time}", DateTime.Now);
-                await Task.Delay(1000 * 60, stoppingToken);
-            }
-        }
-
-        public bool TableExists(Sensor sensor)
+        private static bool TableExists(Logger<Worker> logger, Sensor sensor)
         {
             string tableName = sensor.IP.Replace(".", "");
 
@@ -72,8 +33,13 @@ namespace OSWMontiorService
             return false;
         }
 
-        public void AddSensorData(Sensor sensor)
+        public static void AddEntry(Logger<Worker> logger, Sensor sensor)
         {
+            if (!TableExists(logger, sensor))
+            {
+                CreateTable(logger, sensor);
+            }
+
             string tableName = sensor.IP.Replace(".", "");
 
             using (OleDbConnection db = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.16.0; Data Source = C:\Users\monroett\Desktop\OSWSensors.accdb"))
@@ -102,13 +68,13 @@ namespace OSWMontiorService
             }
         }
 
-        private DateTime GetDateTime(DateTime d)
+        private static DateTime GetDateTime(DateTime d)
         {
             // REMOVES MILLISECONDS. WORKAROUND FOR ERROR 'Data type mismatch in criteria expression'
             return new DateTime(d.Year, d.Month, d.Day, d.Hour, d.Minute, d.Second);
         }
 
-        public void CreateTable(Sensor sensor)
+        private static void CreateTable(Logger<Worker> logger, Sensor sensor)
         {
             string tableName = sensor.IP.Replace(".", "");
 
